5b82b88a4888bae610bb06c94ab45c01741c4ed0
ihor.syrov <igor.syrov@dev-pro.net>
2018-03-11 15:15:28 +0200

    Moved all components into it's own folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { FilterComponent } from './filter/filter.component';
-import { WeatherComponent } from './weather/weather.component';
-import { SocialNetworkWidgetComponent } from './social-network-widget/social-network-widget.component';
-import { MainPhotoComponent } from './main-photo/main-photo.component';
+import { CardComponent } from './components/card/card.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { WeatherComponent } from './components/weather/weather.component';
+import { SocialNetworkWidgetComponent } from './components/social-network-widget/social-network-widget.component';
+import { MainPhotoComponent } from './components/main-photo/main-photo.component';
 
 
 @NgModule({
