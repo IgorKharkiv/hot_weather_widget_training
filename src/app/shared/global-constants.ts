34833b91bb839b1b6a925987f4441155b8db1a21
ihor.syrov <igor.syrov@dev-pro.net>
2018-03-11 22:20:34 +0200

    Fixed placeholder url

--- src/app/shared/global-constants.ts
+++ src/app/shared/global-constants.ts
@@ -1,3 +1,3 @@
 // base app url
 export const baseUrl = window.location.origin + window.location.pathname;
-export const placeholderUrl = 'http://localhost:4200/assets/images/placeholder.jpg';
+export const placeholderUrl = `${baseUrl}assets/images/placeholder.jpg`;
